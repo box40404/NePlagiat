f457570bb72ae2c4bfa7fdf860681999f833f69b
box40404 <pashayashik@mail.ru>
2024-03-03 20:56:13 +0300

    + group factory and seeder

---
+++ database/factories/GroupFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Group>
+ */
+class GroupFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "name" => fake()->slug(2),
+            "followers" => fake()->numberBetween(0, 999999),
+            "description" => fake()->sentence(),
+            "tags" => fake()->word()
+        ];
+    }
+}
