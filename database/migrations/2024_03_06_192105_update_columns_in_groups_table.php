db72105bbbbd3ec774f67392daf04a9199d21da6
box40404 <pashayashik@mail.ru>
2024-03-06 22:42:09 +0300

    change group table

---
+++ database/migrations/2024_03_06_192105_update_columns_in_groups_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('groups', function (Blueprint $table) {
+            $table->integer('followers')->default(0)->change();
+            $table->string('name', 100)->change();
+            $table->foreignIdFor(User::class)->after('tags');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('groups', function (Blueprint $table) {
+            //
+        });
+    }
+};
