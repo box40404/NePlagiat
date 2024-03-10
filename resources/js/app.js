06d6988115e680270ea0e41d277f3fd8ed0daa0a
box40404 <pashayashik@mail.ru>
2024-03-10 22:43:45 +0300

    frontent update

--- resources/js/app.js
+++ resources/js/app.js
@@ -46,3 +46,36 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+
+document.addEventListener('DOMContentLoaded', function() {
+    const viewPhotosButtons = document.querySelectorAll('.view-photos-btn');
+
+    viewPhotosButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            const additionalPhotos = button.parentElement.querySelector('.additional-photos');
+            if (additionalPhotos.style.display === 'none' || additionalPhotos.style.display === '') {
+                additionalPhotos.style.display = 'flex';
+                button.textContent = 'Hide Photos';
+            } else {
+                additionalPhotos.style.display = 'none';
+                button.textContent = 'View Photos';
+            }
+        });
+    });
+});
+
+const fileInput = document.getElementById('file-input');
+const selectedPhotosDiv = document.getElementById('selected-photos');
+
+fileInput.addEventListener('change', function() {
+    selectedPhotosDiv.innerHTML = ''; // Очищаем предыдущие выбранные фотографии
+
+    const files = Array.from(fileInput.files); // Получаем выбранные файлы
+    files.forEach(file => {
+        const image = document.createElement('img');
+        image.src = URL.createObjectURL(file); // Получаем URL для просмотра выбранной фотографии
+        image.alt = file.name; // Устанавливаем имя файла в качестве альтернативного текста
+        image.classList.add('selected-photo');
+        selectedPhotosDiv.appendChild(image); // Добавляем изображение в div
+    });
+});
--- resources/views/components/base.blade.php
+++ resources/views/components/base.blade.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> {{$title}} </title>
-    @vite('resources/css/app.css')
-    @vite('resources/css/create-post.css')
+    {{$style}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+
 </head>
 
 <body>
