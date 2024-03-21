bb3c0a765243b6fcfb5c47f4aa9b78809363bf6f
box40404 <pashayashik@mail.ru>
2024-03-21 16:38:42 +0300

    fix

--- resources/views/components/base.blade.php
+++ resources/views/components/base.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> {{$title}} </title>
     @vite('resources/css/app.css')
-    {{$styles}}
+    {{$styles ?? null}}
 </head>
 
 <body>
@@ -28,7 +28,7 @@
         <p>&copy; 2024 NePlagiat. No rights reserved.</p>
     </footer>
 
-    {{$scripts}}
+    {{$scripts ?? null}}
 
 </body>
 </html>
